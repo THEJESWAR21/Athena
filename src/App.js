12cdcf0ff7503ea6fbcea63adef0e1f56eddeaf7
Thejeswar <64600548+THEJESWAR21@users.noreply.github.com>
2020-10-15 18:50:28 +0530

    Sign-in page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faTwitter, faLinkedin, faInstagram} from "@fortawesome/free-brands-svg-icons"
+import logo from './img/teaching.svg';
+import "./App.css";
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="" className="sign-in-form">
+              <h2 className="title">Sign in</h2>           
+              <div className="input-field">
+                <i>
+                <FontAwesomeIcon icon={faUser} />
+                </i>
+                <input type="text" placeholder="Username"></input>
+              </div>
+              <div className="input-field">
+                <i>
+                <FontAwesomeIcon icon={faLock} />
+                </i>
+                <input type="password" placeholder="Password"></input>
+              </div>
+              <input type="submit" value="Login" className="btn solid">
+              </input>
+              <p className="social-text">Social Platforms</p>
+              <div className="social-media">
+                <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="#" className="social-icon">
+                <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </div>
+               </form>
+
+
+              
+               
+               
+          </div>
+        </div>
+
+        <div className="panels-container">
+        <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h1>Athena Academy</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+              ex ratione. Aliquid!dgs wrkjtrwkj swufshfw ljtyrut iuirwfhlb duwfih rgrht 
+              wregiwhurhh
+              rtrt
+            </p>
+            <button class="btn transparent" id="sign-up-btn">
+              Sign up
+            </button>
+          </div>
+          <img src={logo} alt="Logo" className="image" />
+        </div>
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>One of us ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              laboriosam ad deleniti.
+            </p>
+            <button class="btn transparent" id="sign-in-btn">
+              Sign in
+            </button>
+          </div>
+          <img src="svg 1" class="image" alt="" />
+        </div>
+      </div>
     </div>
+        </div>
+      </div>
   );
 }
 
